10d38bc59b046516d4d2c150f69be5fd825d7ae3
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2022-05-31 23:57:33 -0600

    Search report page added
    Page used to search reports by date and name. It uses the report table component to show the results.

---
+++ src/app/component/page/report/search-report/search-report.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
+import { ReportResponse } from 'src/app/payload/report/report.payload';
+import { ReportService } from 'src/app/service/report/report.service';
+import { SnackbarService } from 'src/app/service/snackbar/snackbar.service';
+import { GET_APP_TEXT } from 'src/app/utils/text';
+
+@Component({
+  selector: 'app-search-report',
+  templateUrl: './search-report.component.html',
+  styleUrls: ['./search-report.component.scss'],
+})
+export class SearchReportComponent implements OnInit {
+  GET_APP_TEXT = GET_APP_TEXT;
+  searchForm: FormGroup;
+  reports: ReportResponse[] = [];
+  constructor(
+    private formBuilder: FormBuilder,
+    private reportService: ReportService,
+    private snackbarService: SnackbarService
+  ) {
+    this.searchForm = this.formBuilder.group({});
+  }
+
+  ngOnInit(): void {
+    this.searchForm = this.formBuilder.group({
+      name: [''],
+      since: [
+        moment().hour(0).minutes(0).seconds(0).milliseconds(0),
+        Validators.required,
+      ],
+      until: [
+        moment().hour(23).minutes(59).seconds(59).milliseconds(999),
+        Validators.required,
+      ],
+    });
+  }
+
+  search() {
+    let name = this.searchForm.get('name')?.value;
+
+    let since = this.searchForm.get('since')?.value.toISOString();
+    let until = this.searchForm.get('until')?.value.toISOString();
+    this.reportService.search(name, since, until).subscribe(
+      (data) => {
+        this.reports = data;
+      },
+      (error) => {
+        this.snackbarService.error(error);
+      }
+    );
+  }
+}

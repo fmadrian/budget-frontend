6d106bdba148d33ff39ee0a199a6e04b1d4c38d9
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2022-05-31 23:39:21 -0600

    Error pages use error message defined in text file.

--- src/app/component/page/internal-error/internal-error.component.ts
+++ src/app/component/page/internal-error/internal-error.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { GET_APP_TEXT } from 'src/app/utils/text';
 
 @Component({
   selector: 'app-internal-error',
   templateUrl: './internal-error.component.html',
-  styleUrls: ['./internal-error.component.scss']
+  styleUrls: ['./internal-error.component.scss'],
 })
 export class InternalErrorComponent implements OnInit {
+  GET_APP_TEXT = GET_APP_TEXT;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/component/page/not-found/not-found.component.ts
+++ src/app/component/page/not-found/not-found.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { GET_APP_TEXT } from 'src/app/utils/text';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent implements OnInit {
+  GET_APP_TEXT = GET_APP_TEXT;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

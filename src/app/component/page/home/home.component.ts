38d2018df8f5e4e1193afcf89fc7333fb31821de
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2022-05-31 23:42:26 -0600

    Home component updated

--- src/app/component/page/home/home.component.ts
+++ src/app/component/page/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { APP_ROUTES } from 'src/app/utils/appRoutes';
 import { GET_APP_TEXT } from 'src/app/utils/text';
 
 @Component({
@@ -7,8 +9,20 @@ import { GET_APP_TEXT } from 'src/app/utils/text';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  title = GET_APP_TEXT('app', 'name');
-  constructor() {}
+  GET_APP_TEXT = GET_APP_TEXT;
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  createReport() {
+    this.router.navigate(['/', APP_ROUTES.report.create], {
+      queryParams: { copy: 'last' },
+    });
+  }
+  createReportBlank() {
+    this.router.navigateByUrl(APP_ROUTES.report.create);
+  }
+  searchReport() {
+    this.router.navigateByUrl(APP_ROUTES.report.search);
+  }
 }
